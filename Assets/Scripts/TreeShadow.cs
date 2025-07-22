11cb9fc0e7deb55182db501730871bd10620a425
ParkerCrandell <parkerdc7@gmail.com>
2025-07-22 13:40:49 -0400

    tree wind improvements

--- Assets/Scripts/TreeShadow.cs
+++ Assets/Scripts/TreeShadow.cs
@@ -20,8 +20,13 @@ public class TreeShadow : MonoBehaviour
         seed = Random.Range(0f, 50f);
     }
     private void Update()
-    {;
-        transform.position += ((start + Vector3.right * Mathf.Sin(seed + Time.time * (Mathf.PerlinNoise1D(Time.time * 0.5f + seed) * valA)) * valB) - (new Vector3(transform.position.x / 2, transform.position.y, transform.position.z))) * 0.1f;
-        child.localPosition += ((childStart + Vector3.right * Mathf.Sin(seed+Time.time * (Mathf.PerlinNoise1D(Time.time * 0.5f + seed) * valChildA)) * valChildB) - ((new Vector3(child.localPosition.x / 2, child.localPosition.y, child.localPosition.z)))) * 0.1f;
+    {
+        float noise = Mathf.PerlinNoise(Time.time * 0.1f + seed, 0f);
+        float offsetX = Mathf.Sin(seed + Time.time * (noise * valA)) * valB;
+        float offsetY = Mathf.Sin(seed/2 + Time.time * (noise * valA)) * valB;
+        float childOffsetX = Mathf.Sin(seed +Time.time * (noise * valChildA)) * valChildB;
+        float childOffsetY = Mathf.Sin(seed/2 + Time.time * (noise * valChildA)) * valChildB;
+        transform.position += ((start + (Vector3.right * offsetX) + Vector3.down * offsetY) - (new Vector3(transform.position.x / 2, transform.position.y, transform.position.z))) * 0.1f;
+        child.localPosition += ((childStart + Vector3.right * childOffsetX + Vector3.down * childOffsetY) - ((new Vector3(child.localPosition.x / 2, child.localPosition.y, child.localPosition.z)))) * 0.1f;
     }
 }
