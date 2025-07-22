6fe98d20e3904bfd9a7da7a52bb6befff7fd7321
ParkerCrandell <parkerdc7@gmail.com>
2025-07-21 22:46:49 -0400

    Tree wind

--- Assets/Scripts/TreeShadow.cs
+++ Assets/Scripts/TreeShadow.cs
@@ -8,6 +8,11 @@ public class TreeShadow : MonoBehaviour
     public Transform child;
     Vector3 childStart;
     float seed;
+    public float valA = 0.2f;
+    public float valB = 0.75f;
+    public float valChildA = 0.3f;
+    public float valChildB = 0.3f;
+
     private void Start()
     {
         start = transform.position;
@@ -15,8 +20,8 @@ public class TreeShadow : MonoBehaviour
         seed = Random.Range(0f, 50f);
     }
     private void Update()
-    {
-        transform.position += ((start + Vector3.right* Mathf.Sin(seed+ Time.time*0.2f)*0.75f)-transform.position) *0.1f;
-        child.localPosition += ((childStart + Vector3.right * Mathf.Sin(seed+Time.time * 0.33f) * 0.3f) - child.localPosition) * 0.1f;
+    {;
+        transform.position += ((start + Vector3.right * Mathf.Sin(seed + Time.time * (Mathf.PerlinNoise1D(Time.time * 0.5f + seed) * valA)) * valB) - (new Vector3(transform.position.x / 2, transform.position.y, transform.position.z))) * 0.1f;
+        child.localPosition += ((childStart + Vector3.right * Mathf.Sin(seed+Time.time * (Mathf.PerlinNoise1D(Time.time * 0.5f + seed) * valChildA)) * valChildB) - ((new Vector3(child.localPosition.x / 2, child.localPosition.y, child.localPosition.z)))) * 0.1f;
     }
 }
