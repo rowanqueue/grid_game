d89271a08f67eec198bb94f2ad9770509d31f6bb
ParkerCrandell <parkerdc7@gmail.com>
2026-07-24 13:41:13 -0400

    New Difficulty Menu

--- Assets/Scripts/TreeShadow.cs
+++ Assets/Scripts/TreeShadow.cs
@@ -25,7 +25,7 @@ public class TreeShadow : MonoBehaviour
     }
     private void Update()
     {
-        noise = Mathf.Clamp(Mathf.PerlinNoise1D(Time.time * 0.1f + seed), 0f, 0.7f);
+        noise = Mathf.Lerp(0.75f, 1.1f, Mathf.PerlinNoise1D(Time.time * 0.1f + seed));
         input = seed + Time.time * valA;
         output = Mathf.Sin(input) * valB * noise;
         float offsetX = output;
